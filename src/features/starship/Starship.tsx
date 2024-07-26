7036fca7f46dacaafe6322f193d74696e84085d1
forgandenny <forgandenny@gmail.com>
2024-07-26 23:13:43 +0100

    feat: starship page layout

--- src/features/starship/Starship.tsx
+++ src/features/starship/Starship.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react"
+import { useState, useCallback, useMemo } from "react"
 import { useParams } from "react-router-dom"
 import {
   Stack,
@@ -6,6 +6,11 @@ import {
   NumberInput,
   Loading,
   ToastNotification,
+  Grid,
+  Column,
+  StructuredListWrapper,
+  StructuredListRow,
+  StructuredListCell,
 } from "@carbon/react"
 
 import { Error } from "../../components/Error"
@@ -24,6 +29,43 @@ export const Starship = () => {
   const [count, setCount] = useState(1)
   const [buyCount, setBuyCount] = useState(1)
   const [showToast, setShowToast] = useState(false)
+
+  const descriptionItems = useMemo(() => {
+    return [
+      {
+        label: "Model",
+        text: data?.model,
+      },
+      {
+        label: "Class",
+        text: data?.starship_class,
+      },
+      {
+        label: "Max Speed",
+        text: data?.max_atmosphering_speed,
+      },
+      {
+        label: "MGLT",
+        text: data?.MGLT,
+      },
+      {
+        label: "Crew",
+        text: data?.crew,
+      },
+      {
+        label: "Passengers",
+        text: data?.passengers,
+      },
+      {
+        label: "Cargo capacity",
+        text: data?.cargo_capacity,
+      },
+      {
+        label: "Consumables",
+        text: data?.consumables,
+      },
+    ]
+  }, [data])
 
   const handleCount = useCallback(
     (
@@ -59,7 +101,7 @@ export const Starship = () => {
 
   if (isSuccess) {
     return (
-      <Stack gap={6}>
+      <>
         {showToast ? (
           <div className={styles.topRight}>
             <ToastNotification
@@ -71,26 +113,51 @@ export const Starship = () => {
               role="status"
               statusIconDescription="notification"
               subtitle="Added to basket"
-              timeout={5000}
+              timeout={2000}
               title={`${buyCount}x ${data.name}`}
             />
           </div>
         ) : null}
-        <h1 aria-label="Starship name">{data.name}</h1>
-        <p>{data.manufacturer}</p>
-        <NumberInput
-          id="carbon-number"
-          min={1}
-          max={10}
-          value={count}
-          label="How many starships to buy (max 10)"
-          invalidText="You can only add beween 1 and 10 starships at a time"
-          onChange={handleCount}
-        />
-        <Button aria-label="Buy starship" type="button" onClick={handleBuy}>
-          BUY
-        </Button>
-      </Stack>
+        <Grid>
+          <Column sm={4} md={8} lg={8}>
+            <Stack gap={6}>
+              <h1 aria-label="Starship name">{data.name}</h1>
+              <p>{data.manufacturer}</p>
+              <div className={styles.heroPadding} />
+            </Stack>
+          </Column>
+          <Column sm={4} md={8} lg={8}>
+            <StructuredListWrapper>
+              {descriptionItems.map(item => (
+                <StructuredListRow key={`bi${item.label}`}>
+                  <StructuredListCell head>{item.label}</StructuredListCell>
+                  <StructuredListCell>{item.text}</StructuredListCell>
+                </StructuredListRow>
+              ))}
+            </StructuredListWrapper>
+            <div className={styles.buyContainer}>
+              <Stack gap={2}>
+                <NumberInput
+                  id="carbon-number"
+                  min={1}
+                  max={10}
+                  value={count}
+                  label="How many starships to buy (max 10)"
+                  invalidText="You can only add beween 1 and 10 starships at a time"
+                  onChange={handleCount}
+                />
+                <Button
+                  aria-label="Buy starship"
+                  type="button"
+                  onClick={handleBuy}
+                >
+                  BUY
+                </Button>
+              </Stack>
+            </div>
+          </Column>
+        </Grid>
+      </>
     )
   }
 
